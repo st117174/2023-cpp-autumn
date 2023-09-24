9c98f40adb40aa17401a7155d142d836f7dc0768
st117174 <st117174@student.spbu.ru>
2023-09-24 22:37:56 +0300

    Lesson 1 code update

--- Lesson2.HW.2023.09.23/project4/project4.cpp
+++ Lesson2.HW.2023.09.23/project4/project4.cpp
@@ -9,10 +9,57 @@ int main(int argc, char* argv[])
 	int d = 0;
 	int e = 0;
 	e = n / 60;
-	d = (n - e * 60) / 20;
-	c = (n - e * 60 - d * 20) / 10;
-	b = (n - e * 60 - d * 20 - c * 10) / 5;
-	a = (n - e * 60 - d * 20 - c * 10 - b * 5);
+	n -= 60 * e;
+	if (n >= 36)
+	{
+		e++;
+	}
+	else if (n <= 35 && n >= 20)
+	{
+		d++;
+		n -= 20;
+		c = n / 10;
+		n -= 10 * c;
+		if (n == 9)
+		{
+			c = 1;
+		}
+		if (n != 9)
+		{
+			b = n / 5;
+			n -= 5 * b;
+			a = n;
+		}
+	}
+	else if (n <= 19)
+	{
+		if (n == 19 || n == 18)
+		{
+			d++;
+		}
+		else if (n != 19 && n != 18)
+		{
+			c = n / 10;
+		n -= c * 10;
+		if (n == 9)
+		{
+			c++;
+		}
+		else if (n != 9)
+		{
+			if (n < 5)
+			{
+				a = n;
+			}
+			else if (n > 5)
+			{
+				b++;
+				n -= 5;
+				a = n;
+			}
+		}
+	}
+	}
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << c << std::endl;
---
+++ Lesson2.HW.2023.09.23/project6/project6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>;
+int main(int argc, char* argv[])
+{
+	int k = 0;
+	int m = 0;
+	int n = 0;
+	std::cin >> k;
+	std::cin >> m;
+	std::cin >> n;
+	int a = 0;
+	int l = 0;
+	int p = 0;
+	if (n == 0 || m == 0|| k == 0)
+	{
+		a = 0;
+	}
+	else if (n <= k)
+	{
+		a = 2 * m;
+	}
+	else if (n > k)
+	{
+		l = n / k;
+		p = n % k;
+		if (p > 0)
+		{
+			a = 2 * l * m + m;
+		}
+		else if (p == 0)
+		{
+			a = 2 * l * m;
+		}
+	}
+	std::cout << a << std::endl;
+	return EXIT_SUCCESS;
+}
