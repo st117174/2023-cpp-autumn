fe0ca23a45f7c1db387f88127674f21224b1b21c
st117174 <st117174@student.spbu.ru>
2023-12-03 21:27:47 +0300

    Lesson10 upload

---
+++ Lesson10.HW.2023.23.11/project6/project6.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+void hanoi(int n, int from, int to);
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	std::cin >> a;
+	hanoi(a, 1, 3);
+	return EXIT_SUCCESS;
+}
+
+void hanoi(int n, int from, int to)
+{
+	if (n > 0)
+	{
+		int tmp = 6 - from - to;
+		if (n == 1)
+		{
+			printf("1 %d %d\n", from, to);
+		}
+		else
+		{
+			hanoi(n - 1, from, to);
+			hanoi(n - 2, to, tmp);
+			printf("0 %d %d\n", from, to);
+			hanoi(n - 2, tmp, from);
+			hanoi(n - 1, from, to);
+		}
+	}
+}
