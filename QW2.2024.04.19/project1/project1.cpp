c1159523c1f22174ef5d1fee0d051afffd2ff087
st117174 <st117174@student.spbu.ru>
2024-04-19 14:34:02 +0300

    Hometest.upload

--- QW2.2024.04.19/project1/project1.cpp
+++ QW2.2024.04.19/project1/project1.cpp
@@ -38,51 +38,176 @@ public:
 	//вставляет элемент data на позицию index
 	void Insert(int index, int data)
 	{
-
+		Node* node = new Node(data);
+		InsertNode(index, node);
 	}
 
 	void PushHead(int data)
 	{
-
+		Node* node = new Node(data);
+		InsertNode(0, data);
 	}
 
 	void PushTail(int data);
+	{
+		Node* node = new Node(data);
+		InsertNode(Length(), data);
+	}
 
 	//извлекает головной элемент
-	int PopHead();
+	int PopHead()
+	{
+		return PopData(ExtractNode(0));
+	}
 
 	//извлекает данные из позиции index
 	//и удаляет элемент из списка
-	int Extract(int index);
-	int PopTail();
+	int Extract(int index)
+	{
+		return PopData(ExtractNode(index));
+	}
+
+
+	int PopTail()
+	{
+		return PopData(ExtractNode(index));
+	}
 
 	//получает данные из позиции index
 	//без удаления из списка
-	int Data(int index);
-	void swap(int ia, int ib);
-	void sort();
+	int Data(int index)
+	{
+		Node* cur = head;
+		for (int i = 0, i < index&& cur != nullptr; ++i)
+		{
+			cur = cur->next;
+		}
+		if (cur != nullptr)
+		{
+			return cur->data;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	void Swap(int ia, int ib)
+	{
+		if (ia == ib || !IndexValid(ia) || !IndexValid(ib))
+		{
+			return;
+		}
+		if (ia > ib)
+		{
+			return swap(ib, ia)
+		}
+		Node* na = ExtractNode(ia);
+		Node* nb = Excract(ib - 1);
+		if (na = nullptr || nb == nullptr)
+		{
+			return;
+		}
+		InsertNode(ia, nb);
+		InsertNode(ib, na);
+	}
+
+	void sort()
+	{
+		for (int i = Length() - 1; i >= 0; --i)
+		{
+			for (int j = 0, j < i; ++j)
+			{
+				if Data(j) > Data(j + 1) {
+					Swap(j, j + 1)
+				}
+			}
+		}
+	}
 
 	friend std::ostream& operator<<(std::ostream& stream, const LinkedList& list)
 	{
-
-	}
-		;
+		Node* cur = list.head;
+		while (cur != nullptr)
+		{
+			stream << *cur << " ";
+			cur = cur->next;
+		}
+		return stream;
+	};
 
 private:
-	void dispose();
+	void dispose()
+	{
+		while (head != nullptr)
+		{
+			Node* tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+	}
 
 	//извлекает данные из ноды и удаляет саму ноду
 	int PopData(Node* node)
-	{}
+	{
+		if (node == nullptr)
+		{
+			return 0;
+		}
+		int res = node->data;
+		delete node;
+		return res;
+	}
 
 	void InsertNode(int index, Node* node)
-	{}
+	{
+			if (index <= 0 || head == nullptr)
+			{
+				node->next = head;
+				head = node;
+			}
+			else
+			{
+				Node* cur = head;
+				for (int i = 0; i < index - 1 && cur->next != nullptr; ++i)
+				{
+					cur = cur->next;
+				}
+				node->next = cur->next;
+				cur->next = node;
+			}
+	}
 
 	Node* ExtractNode(int index)
-	{}
+	{
+		If(IndexValid(index))
+		{
+			if (index == 0)
+			{
+				Node* tmp = head;
+				head = head->next;
+				return tmp;
+			}
+			Node* cur = head;
+			for (int i = 0; i < index - 1; ++i)
+			{
+				cur = cur->next;
+			}
+			Node* tmp = cur->next;
+			cur->next = tmp->next;
+			return tmp;
+		}
+		return nullptr;
+	}
 
 	bool IndexValid(int index)
-	{}
+	{
+		if (index >= 0)
+		{
+			return index < Length();
+		}
+		return false;
+	}
 
 	Node* head;
 };
